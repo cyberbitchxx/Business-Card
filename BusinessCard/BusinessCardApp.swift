5cb8516cb5499945d8f99a246fbbbc1b088437c3
cyberbitchxx <shchukinasofiya@gmail.com>
2022-08-02 17:24:08 +0300

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by sofia on 02.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
